70e3df76ae1a7c2a36385ff79bb4e07b719e3c65
Héliton Oliveira <heliton.oliveira88@gmail.com>
2020-08-10 15:46:56 -0300

    page main done

---
+++ src/pages/Main/index.jsx
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
+
+import api from '../../services/api';
+
+import { Container, SubmitButton, Form, List } from './styled';
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newRepo: '',
+      repositories: [],
+      loading: false,
+    };
+  }
+
+  // carregar os dados do localStorage
+  componentDidMount() {
+    const repositories = localStorage.getItem('@repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  // salvar os dados no localStorage
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('@repositories', JSON.stringify(repositories));
+    }
+  }
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+
+    const { newRepo, repositories } = this.state;
+
+    const response = await api.get(`repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    });
+  };
+
+  render() {
+    const { newRepo, loading, repositories } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithub size={30} />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#fff" size={14} />
+            ) : (
+              <FaPlus color="#fff" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+
+        <List>
+          {repositories.map(repo => (
+            <li key={repo.name}>
+              {repo.name}
+              <Link to={`/repository/${encodeURIComponent(repo.name)}`}>
+                Detalhes
+              </Link>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
+}
+
+export default Main;
---
+++ src/pages/Repository/index.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import api from '../../services/api';
+
+class Repository extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      repository: {},
+      issues: [],
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repo);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { loading, issues, repository } = this.state;
+
+    return <h1>Repository</h1>;
+  }
+}
+
+export default Repository;
